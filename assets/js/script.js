a5d2fe43ec343d56f866fad804c710ba6905265e
Christian Sanchez <csanchezmartinez486@gmail.com>
2022-02-03 22:24:49 -0500

    created variables in script.js that reference the ids from the index.html

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,4 +1,21 @@
-//links timerEl to index.html
-var timerEl = document.querySelector(".timer")
+// button: starts quiz 
+var startBtn = document.getElementById("startBtn");
 
-v
+// section of the index page
+var gamePg = document.getElementById("game");
+var quiz = document.getElementById("quiz");
+var score = document.getElementById("score");
+var scoreBoard = document.getElementById("scoreBoard")
+
+
+
+
+
+//links timerEl to index.html
+var timerEl = document.querySelector(".timer");
+
+// timer will countdown from 30 secs
+function timer() {
+    var secLeft = 60;
+}
+
